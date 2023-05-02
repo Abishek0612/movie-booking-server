bf749ec910510c9d4f60f6a6a16498a3c46b997c
Abishek0612 <uabishek6@gmail.com>
2023-05-03 02:31:38 +0530

    done with register and login API end points

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ require('dotenv').config()
 require('./db')
 app.use(express.json());
 app.use(morgan("dev"))
+
+
+//routes
+const userRoute = require("./routes/usersRoute")
+
+app.use('/api/users', userRoute)
+
 const port = process.env.PORT ||5000;
 
 app.listen(port,() => console.log(`Server started and running in port ${port}`))
